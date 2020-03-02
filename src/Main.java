c8c5d63fae73c816025c7834a521887f959595a5
gorgor1000 <pribytkingorde@gmail.com>
2020-03-03 00:05:15 +0200

    resolved conflict

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("we're both here now...");
+        System.out.println("We're both here now");
     }
 }
